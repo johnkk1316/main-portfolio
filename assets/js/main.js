bd99a8c62786b0b68ad62eb4032284c1fa8d1fc2
John KK <johnkk1316@protonmail.com>
2022-09-03 22:35:25 +0300

    Added mixitup js file and also added functionality to the work sector.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,24 @@ modalCloses.forEach((mc) => {
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active work */ 
+const  linkWork = document.querySelectorAll('.work__item')
 
+function activeWork(){
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
