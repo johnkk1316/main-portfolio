badd07386a5ac065c96b3f3e46cd3988abc06424
John KK <johnkk1316@protonmail.com>
2022-09-03 17:09:39 +0300

    Added functionality to services sector services modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,26 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const   modalViews = document.querySelectorAll('.services__modal')
+        modalBtns = document.querySelectorAll('.services__button')
+        modalCloses = document.querySelectorAll('.services__modal-close')
+let     modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
