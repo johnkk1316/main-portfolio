3c4a85f2b1206551a6ff1b23b384ede7e8306210
John KK <johnkk1316@protonmail.com>
2022-09-04 20:36:11 +0300

    added swiper functionality to testimonial sector

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,25 @@ function activeWork(){
 linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+    },
+}); 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
